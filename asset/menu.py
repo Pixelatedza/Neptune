2bec2bf49d996cef08fe64cb366d467938431486
Pixelatedza <ekpoepol@gmail.com>
2016-02-06 20:16:18 +0200

    Menu Initial
    Added menu objects with register function. It is now possible to register menus from any application in the installed apps list. Simply create a menu.py and import nepcore.menu.
    
    Then menu.register(*args, **kwargs)
    
    Also with this is the base.html that can be inherited from.

--- asset/menu.py
+++ asset/menu.py
@@ -1,5 +1,6 @@
-from nepcore.menu import MenuObj, hello_menu, menu
+from nepcore.menu import MenuObj, hello_menu, menu, other_menu
 
-menu = menu.register(MenuObj(link="hello asset",text="Hello Asset"))
-hello_menu.register(MenuObj(link="child 1 asset",text="Child 1 Asset"))
-hello_menu.register(MenuObj(link="child 2 asset",text="Child 2 Asset"))
+menu = menu.register(MenuObj(link="hello_asset",text="Hello Asset"))
+hello_menu.register(MenuObj(link="child_1_asset",text="Child 1 Asset"))
+hello_menu.register(MenuObj(link="child_2_asset",text="Child 2 Asset"))
+other_menu.register(MenuObj(link="other_child_2_asset",text="Other Child 2 Asset"))
--- nepcore/menu.py
+++ nepcore/menu.py
@@ -6,11 +6,11 @@ from django import template
 
 class Menu(object):
 	template_path = 'nepcore/menus/menu.html'
+	location = 'main_nav'
 
 	def __init__(self, *args, **kwargs):
 		self.children = []
 		self.parent = kwargs.get('menu', None)
-		self.location = kwargs.get('location', None)
 
 	def auto_discover(self):
 		for app in settings.INSTALLED_APPS:
@@ -31,13 +31,17 @@ class Menu(object):
 		return menu_obj
 
 	def render(self):
-		template = get_template(self.template_path)
-		return template.render()
+		tmpl = get_template(self.template_path)
+		context = template.Context({'menu': self})
+		html = tmpl.render(context)
+		print self.children
+		return html
 
 	def __str__(self):
 		return self.render()
 
 class MenuObj(Menu):
+	template_path = 'nepcore/menus/menu_obj.html'
 
 	def __init__(self, *args, **kwargs):
 		super(MenuObj, self).__init__(*args, **kwargs)
@@ -47,10 +51,12 @@ class MenuObj(Menu):
 	def __str__(self):
 		return self.text
 
-menu = Menu(location="main_nav")
+menu = Menu()
 hello_menu = menu.register(MenuObj(link="hello",text="Hello Menu"))
-hello_menu.register(MenuObj(link="child 1",text="Child 1"))
-hello_menu.register(MenuObj(link="child 2",text="Child 2"))
+hello_menu.register(MenuObj(link="child_1",text="Child 1"))
+hello_menu.register(MenuObj(link="child_2",text="Child 2"))
 other_menu = menu.register(MenuObj(link="other",text="Other Menu"))
+lvl = other_menu.register(MenuObj(link="other_child_2",text="Other Child 2"))
+lvl.register(MenuObj(link="lvl_1",text="Level 1"))
 
 menu.auto_discover()
