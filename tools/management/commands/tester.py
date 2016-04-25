cc23653bcfa6a5025293ae26c153041ffd90bd72
Herco <herco.vandyk@gmail.com>
2016-04-25 21:47:55 +0200

    Added apps.py for item.
    Functions implemented to add item objects.

--- tools/management/commands/tester.py
+++ tools/management/commands/tester.py
@@ -3,8 +3,13 @@ import os, sys, time
 
 from django.conf import settings
 
+from item.models import ItemType, Item, Property, ItemProperty, ItemPropertyValue
+
 class Command(BaseCommand):
     help = """Add testing data and test stuff."""
+
+    def create_item_prop_relation(self, itemID, PropertyID):
+        ItemProperty.objects.create()
 
     def handle(self, *args, **options):
 
