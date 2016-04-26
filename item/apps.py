58b092f214a06bf72870c5f13209508ed8615a07
Herco <herco.vandyk@gmail.com>
2016-04-27 01:43:45 +0200

    Extensive work in apps and pop_db
    Items controller now functions on class instances. An instance need to
    be created to access public methods. Gets are still available without
    an instance.
    
    pop_db now contains all gets and creates.
    
    To Do: Update functions.

--- item/apps.py
+++ item/apps.py
@@ -2,62 +2,38 @@ from django.conf import settings
 
 from item.models import ItemType, Item, Attribute, ItemAttribute, ItemAttributeValue
 
-class HandleItems(object):
+class Items(object):
 
-    @classmethod
-    def create_itemType(self, name, description):
-        obj = ItemType(name=name, description=description)
+    def _create_item_attr_relation(self, itemType, attr):
+        obj = ItemAttribute(itemType=itemType, attribute=attr)
         obj.save()
-        return obj
 
-    @classmethod
-    def create_item(self, name, itemType):
+    def _create_item_attr_value(self, item, attr, value):
+        obj = ItemAttributeValue(item=item, attribute=attr, value=value)
+        obj.save()
+
+class HandleItems(Items):
+
+    def __init__(self):
+        self.errors = []
+
+    #private methods
+    def _create_item(self, name, itemType):
         obj = Item(name=name, itemType=itemType)
         obj.save()
         return obj
 
-    @classmethod
-    def create_attribute(self, label, dataType):
-        obj = Attribute(label=label, dataType=dataType)
-        obj.save()
-        return obj
-
-    @classmethod
-    def create_item_attr_relation(self, itemType, attr):
-        obj = ItemAttribute(itemType=itemType, attribute=attr)
-        obj.save()
-
-    @classmethod
-    def create_item_attr_value(self, item, attr, value):
-        obj = ItemAttributeValue(item=item, attribute=attr, value=value)
-        obj.save()
-
-    #Following class methods to be used in other modules and apps. Above methods intended for internal use of this module.
-    @classmethod
+    #public methods
     def create_item_with_attrs(self, input):
     #creates item and populate its properties values.
         itemT = ItemType.objects.get(id=input['itemTypeID'])
-        item = self.create_item(input['itemName'], itemT)
+        item = self._create_item(input['itemName'], itemT)
 
         attributes = input['attributes']
 
         for attr in attributes:
             attribute = Attribute.objects.get(id=attr['id'])
-            self.create_item_attr_value(item, attribute, attr['value'])
-
-    @classmethod
-    def create_type_with_attrs(self, input):
-    #creates a new item type and its attributes and links them.
-        try:
-
-            itemType = self.create_itemType(input['itemType'], input['itemType'])
-            for attr in input['attributes']:
-                newAttr = self.create_attribute(attr['label'], attr['dataType'])
-                self.create_item_attr_relation(itemType, newAttr)
-
-            return True
-        except:
-            return False
+            self._create_item_attr_value(item, attribute, attr['value'])
 
     @classmethod
     def get_item_values(self, itemID):
@@ -74,6 +50,50 @@ class HandleItems(object):
         return values
 
     @classmethod
+    def get_all_items(self):
+        result = {}
+        for item in Item.objects.all():
+            result[item.id] = item.name
+        return result
+
+class HandleItemTypes(Items):
+
+    # def __init__(self):
+    #     self.errors = []
+
+    #private methods
+    def _create_itemType(self, name, description):
+        obj = ItemType(name=name, description=description)
+        obj.save()
+        return obj
+
+    def _create_attribute(self, label, dataType):
+        obj = Attribute(label=label, dataType=dataType)
+        obj.save()
+        return obj
+
+
+    #public methods
+    def create_type_with_attrs(self, input):
+    #creates a new item type and its attributes and links them.
+        try:
+            itemType = self._create_itemType(input['itemType'], input['itemType'])
+            for attr in input['attributes']:
+                newAttr = self._create_attribute(attr['label'], attr['dataType'])
+                self._create_item_attr_relation(itemType, newAttr)
+            return True
+        except:
+            return False
+
+    @classmethod
+    def get_all_item_types(self):
+    #return all item types: {itemTypeID:ItemTypeName}
+        result = {}
+        for itemType in ItemType.objects.all():
+            result[itemType.id] = itemType.name
+        return result
+
+    @classmethod
     def get_item_type_attrs(self, itemTypeID):
     #expects an itemType ID
     #returns a dict with attribute types: {attribute id: attribute label)
@@ -85,23 +105,3 @@ class HandleItems(object):
             result[itAt.attribute.id] = itAt.attribute.label
 
         return result
-
-    @classmethod
-    def get_all_item_types(self):
-    #return all item types: {itemTypeID:ItemTypeName}
-        result = {}
-        for itemType in ItemType.objects.all():
-            result[itemType.id] = itemType.name
-        return result
-
-    @classmethod
-    def get_all_items(self):
-        result = {}
-        for item in Item.objects.all():
-            result[item.id] = item.name
-        return result
-
-
-
-class HandleItemTypes(object):
-    pass
--- tools/management/commands/pop_db.py
+++ tools/management/commands/pop_db.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from item.apps import HandleItems
+from item.apps import HandleItems, HandleItemTypes
     #create_type_with_attrs, get_item_type_attrs, get_item_values,create_item_with_attrs
 
 from django.conf import settings
@@ -16,40 +16,43 @@ class Command(BaseCommand):
                                      {'label': 'weight', 'dataType': 'int'},
                                      {'label': 'colour', 'dataType': 'str'}]
                       }
-        #create taskType and Attribute example:
-        # if HandleItems.create_type_with_attrs(FRIDGE):
+        # create taskType and Attribute example:
+        # typeHandler = HandleItemTypes()
+        # if typeHandler.create_type_with_attrs(FRIDGE):
         #     print 'Yeeeahhhh!'
 
 
         #retrieval of attributes for an item type example:
-        # item = HandleItems.get_all_item_types()
-        # itemName = item.itervalues().next()
-        #
-        # for key, value in item.iteritems():
-        #     if value == itemName:
-        #         itemID = key
-        #
-        # attrs = HandleItems.get_item_type_attrs(itemID)
-        #
-        # for attr in attrs:
-        #     print attr, attrs[attr]
-        #
+        typeHandler = HandleItemTypes()
+        item = typeHandler.get_all_item_types()
+        itemName = item.itervalues().next()
+
+        for key, value in item.iteritems():
+            if value == itemName:
+                itemID = key
+
+        attrs = typeHandler.get_item_type_attrs(itemID)
+
+        for attr in attrs:
+            print attr, attrs[attr]
+
         # #Example of an item create:
-        # newItem = {'itemTypeID': itemID}
-        # newItem['itemName'] = 'TaTa'
-        # newItem['attributes'] = []
-        # for attr in attrs:
-        #     newItem['attributes'].append({'id' : attr, 'value' : 'not too sure what val'})
-        #
-        # HandleItems.create_item_with_attrs(newItem)
+        newItem = {'itemTypeID': itemID}
+        newItem['itemName'] = 'TaTa'
+        newItem['attributes'] = []
+        for attr in attrs:
+            newItem['attributes'].append({'id' : attr, 'value' : 'not too sure what val'})
+
+        itemHandler = HandleItems()
+        itemHandler.create_item_with_attrs(newItem)
 
         #Example of getting values for an item:
-        items = HandleItems.get_all_items()
+        items = itemHandler.get_all_items()
         item = items.itervalues().next()
 
         for key, value in items.iteritems():
             if value == item:
                 itemID = key
-        values = HandleItems.get_item_values(itemID)
+        values = itemHandler.get_item_values(itemID)
 
         print values
