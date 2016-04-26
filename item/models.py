9645ab66edd5c938b31d61c888f5ebec14c5142c
Herco <herco.vandyk@gmail.com>
2016-04-26 21:33:14 +0200

    Updated Attribute
    Attribute now has choice list for data types.

--- item/models.py
+++ item/models.py
@@ -2,23 +2,27 @@ from django.db import models
 
 class ItemType(models.Model):
     #different item types
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 class Item(models.Model):
     #different items
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
     itemType = models.ForeignKey(ItemType)
     def __str__(self):
         return self.name
 
 class Attribute(models.Model):
     #different properties
-    label = models.CharField(max_length=255)
-    dataType = models.CharField(max_length=255)
+    ITEM_TYPES = (('str', 'String'),
+                  ('int', 'Integer'),
+                  ('date', 'Date'))
+
+    label = models.CharField(max_length=50)
+    dataType = models.CharField(max_length=50, choices=ITEM_TYPES, default='str')
 
     def __str__(self):
         return self.name
@@ -35,7 +39,7 @@ class ItemAttributeValue(models.Model):
     #which value belong to a property for a specific item.
     item = models.ForeignKey(Item)
     attribute = models.ForeignKey(Attribute)
-    value = models.CharField(max_length=255)
+    value = models.CharField(max_length=50)
 
     def __str__(self):
         return self.itemID
