036cd0ad0432abcee0b8ead06f2ecc10fdbcf9ca
Herco <herco.vandyk@gmail.com>
2016-04-26 21:00:20 +0200

    learn git
    Learning git comparisons on conflicts.

---
+++ Neptune/local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'neptune_git',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
