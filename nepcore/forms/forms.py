6474e412c2b9b66fc18c5691401b379b4e5fbe68
Justin Eloff <ekpoepol@gmail.com>
2016-04-27 21:40:15 +0200

    Form errors
    Item Type form now handles errors.
    
    NEPForms will now have the html to handle forms, some asumptions made on
    what the $scope.formErrors should look like. If $scope.formErrors
    doesn't follow this standard the NEPForms errors will not work.
    
    formErrors = {"field-name": "error msg"}
    
    The above can be ignored for dynamic forms, as they are manually written
    in html for the time being. However dynamic forms errors should be under
    the "attributes" key of the formErrors array, sorted by their index.

--- nepcore/forms/forms.py
+++ nepcore/forms/forms.py
@@ -7,11 +7,14 @@ from django.utils.html import format_html
 class NEPBoundField(BoundField):
 	def label_tag(self, contents=None, attrs=None, label_suffix=None):
 		attrs = {'class':'col-xs-2 control-label'}
-		return super(NEPBoundField, self).label_tag(contents, attrs, label_suffix)
+		return 'ng-class={"has-error":formErrors.' + self.name + '}>' + super(NEPBoundField, self).label_tag(contents, attrs, label_suffix)
 
 	def as_widget(self, widget=None, attrs=None, only_initial=False):
 		attrs = {'class':'form-control', 'ng-model':'formData.' + self.name}
-		return super(NEPBoundField, self).as_widget(widget, attrs, only_initial)
+		error_html = """<div ng-class="{'bg-red color-palette nep-error':formErrors.""" + self.name + """}">
+			<span>{[{formErrors.""" + self.name + """}]}</span>
+		</div>"""
+		return super(NEPBoundField, self).as_widget(widget, attrs, only_initial) + error_html
 
 class NEPBaseForm(BaseForm):
 
@@ -29,7 +32,7 @@ class NEPBaseForm(BaseForm):
 	def as_bootstrap(self):
 		"Returns this form rendered as HTML <tr>s -- excluding the <table></table>."
 		return self._html_output(
-			normal_row='<div class="row form-group">%(label)s<div class="col-xs-10">%(errors)s%(field)s%(help_text)s</div></div>',
+			normal_row='<div class="row form-group"%(label)s<div class="col-xs-10">%(errors)s%(field)s%(help_text)s</div></div>',
 			error_row='<tr><td colspan="2">%s</td></tr>',
 			row_ender='</td></tr>',
 			help_text_html='<br /><span class="helptext">%s</span>',
