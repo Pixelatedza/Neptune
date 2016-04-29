55be474295cee40ad2b46fd177096ddfeb0de66c
Herco <herco.vandyk@gmail.com>
2016-04-29 14:38:56 +0200

    Added create_items methods for items.
    Fixed various error in apps.py. Also cleaned up a lot of code and
    corrected my return dicts.
    
    Added create items and validation on those fields.
    
    Can now also retrieve all items, items of a specific type and all item
    types along with their fields. :D

--- item/apps.py
+++ item/apps.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from nepcore.forms.fields import CUSTOM_FIELD_MAP
 
 from item.models import ItemType, Item, Attribute, ItemAttribute, ItemAttributeValue
 
@@ -12,10 +13,94 @@ class Items(object):
         obj = ItemAttributeValue(item=item, attribute=attr, value=value)
         obj.save()
 
+    def _is_valid_data(self, dataType, data):
+        try:
+            field = CUSTOM_FIELD_MAP[dataType]()
+            field.clean(data)
+            return True
+        except:
+            return False
+
 class HandleItems(Items):
 
-    def __init__(self):
+    def __init__(self, data):
+        self.data = data
         self.errors = []
+
+    def _validate_attributes(self, data):
+        errors = {}
+        for attr in data:
+            attr_errors= {}
+            id = '?'
+            value= ''
+
+            for k, v in attr.iteritems():
+                if k == 'id':
+                    id = v
+                elif k == 'value':
+                    value = v
+
+            if id == '?':
+                attr_errors['id'] = 'No ID for attribute given..'
+
+            if not value:
+                attr_errors['value'] = 'No value given for attribute.'
+
+            if attr_errors:
+                errors[id] = attr_errors
+            else:
+                attribute = Attribute.objects.get().filter(id=id)
+                if not self._is_valid_data(attribute.type, value):
+                    attr_errors['value'] = '%s not a valid value of %s' % (value, attribute.type)
+                    errors[id] = attr_errors
+
+        return data, errors
+
+    def is_valid(self):
+        #validate input for item creation.
+
+        self.errors = {}
+        validated = True
+
+        #expected input:
+        # {
+        #     'itemType': 'name',
+        #     'itemName': 'name',
+        #     'attributes': [{
+        #         'id': 0,
+        #         'value': 'value'
+        #     },{
+        #         'id': 1,
+        #         'value': 'value'
+        #     }]
+        # }
+
+        if 'itemType' in self.data:
+            try:
+                ItemType.objects.get(name=self.data['itemType'])
+                self.errors['itemType'] ='itemType already exists.'
+                validated = False
+            except:
+                pass
+        else:
+            self.errors['itemType'] = 'No itemType given.'
+            validated = False
+
+        if 'itemName' not in self.data:
+            self.errors['itemName'] = 'No itemName given.'
+            validated = False
+
+        data2, attrErrors = self._validate_attributes(self.data['attributes'])
+        self.data['attributes'] = data2
+        if attrErrors:
+            self.errors['attributes'] = attrErrors
+            validated = False
+
+        if validated:
+            self._create_item_with_attributes(self, self.data)
+            return True
+        else:
+            return False
 
     #private methods
     def _create_item(self, name, itemType):
@@ -24,12 +109,12 @@ class HandleItems(Items):
         return obj
 
     #public methods
-    def create_item_with_attrs(self, input):
+    def _create_item_with_attributes(self, data):
     #creates item and populate its properties values.
-        itemT = ItemType.objects.get(id=input['itemTypeID'])
-        item = self._create_item(input['itemName'], itemT)
+        itemT = ItemType.objects.get(name=data['itemType'])
+        item = self._create_item(data['itemName'], itemT)
 
-        attributes = input['attributes']
+        attributes = data['attributes']
 
         for attr in attributes:
             attribute = Attribute.objects.get(id=attr['id'])
@@ -37,24 +122,39 @@ class HandleItems(Items):
 
     @classmethod
     def get_item_values(self, itemID):
-    #returns a dict of attribute values for an item; {attribute label: value}
+    #returns a dict of field values for an item
         item = Item.objects.get(id=itemID)
-        values = {}
+        fields = []
         objs = ItemAttributeValue.objects.all().filter(item=item)
 
         for obj in objs:
-            valueLabel =  obj.attribute.label
-            value = obj.value
-            values[valueLabel] = value
+            fields.append({'label': obj.attribute.label,
+                           'value': obj.value})
 
-        return values
+        return {'item': item.name, 'fields': fields}
 
     @classmethod
     def get_all_items(self):
-        result = {}
+        #Returns all items in the db.
+        returnItems = []
         for item in Item.objects.all():
-            result[item.id] = item.name
-        return result
+            returnItems.append({'itemID': item.id,
+                                'itemName': item.name,
+                                'itemType': item.itemType.name})
+
+        return {'items': returnItems}
+
+    @classmethod
+    def get_all_items_for_type(self, itemTypeName):
+        #Return all items of a specified item type.
+        returnItems = []
+        itemType = ItemType.objects.get(name=itemTypeName)
+
+        for item in Item.objects.all().filter(itemType=itemType):
+            returnItems.append({'itemID': item.id,
+                                'itemName': item.dataType})
+
+        return {'items': returnItems}
 
 class HandleItemTypes(Items):
 
@@ -63,6 +163,8 @@ class HandleItemTypes(Items):
         self.data = data
 
     def _validate_attributes(self, data):
+        #validate attributes when new ones are created.
+        #Code needs revision. Crude, but works. Can be better. :'(
         errors = {}
         for attr in data:
             index = 0
@@ -86,20 +188,23 @@ class HandleItemTypes(Items):
             if not label:
                 attr_errors['label'] = 'No label given.'
 
+            dataType = True
             if not dataType:
                 attr_errors['dataType'] = 'No dataType given.'
+                dataType = False
             elif dataType not in ['str', 'int', 'dat', 'tim']:
-                print dataType
                 attr_errors['dataType'] = 'dataType not allowed.'
-
-            if not required:
-                attr_errors['required'] = 'No required given.'
+                dataType = False
 
             if not default:
                 attr['default'] = ''
             else:
-                #TODO VALIDATE AGAINST DATA TYPE
-                pass
+                if dataType:
+                    if not self._is_valid_data(dataType, default):
+                        attr_errors['default'] = 'Default value %s not of type %s' % (default, dataType)
+
+            if not required:
+                attr_errors['required'] = 'No required given.'
 
             if attr_errors:
                 errors[index] = attr_errors
@@ -119,6 +224,9 @@ class HandleItemTypes(Items):
                 validated = False
             except:
                 pass
+        else:
+            self.errors['itemType'] = 'No itemType given.'
+            validated = False
 
         data2, attrErrors = self._validate_attributes(self.data['attributes'])
         self.data['attributes'] = data2
@@ -168,6 +276,7 @@ class HandleItemTypes(Items):
     def get_item_type_attrs(self, itemTypeName):
     #expects an itemType name
     #returns a dict with attribute types as follows:
+    # {'itemTypeId': 0, 'fields': [{'fieldId': 0, 'dataType': 'str', 'label': 'Name'}]}
 
         fields = []
         itemType = ItemType.objects.get(name=itemTypeName)
